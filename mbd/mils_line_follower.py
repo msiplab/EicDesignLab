4038e3fd39ac040904cebacebc980252edd7ba88
ShoDiMaggio <shogo@eng.niigata-u.ac.jp>
2019-11-11 17:54:41 +0900

    Pygameを利用した画面表示実装

--- mbd/mils_line_follower.py
+++ mbd/mils_line_follower.py
@@ -22,3 +22,59 @@
 
 「電子情報通信設計製図」新潟大学工学部工学科電子情報通信プログラム
 """
+import pygame
+import sys
+import transitions
+
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+
+class LFControler:
+    """ ライントレース制御クラス 
+        
+        ライントレース制御アルゴリズムを実装する。
+
+        入力　フォトリフレクの値 [0,1]x4
+        出力　モーター制御信号 [-1,1]x2
+    """
+    pass
+
+class LFPhisicalModel:
+    """ ライントレーサー物理モデルクラス 
+        
+        ライントレーサーの物理モデを構築する。
+
+        入力　モーター制御信号 [-1,1]x2        
+        出力　フォトリフレクタの値 [0,1]x4 
+    """    
+    pass
+
+class LFMils:
+    """ ライントレースMILSクラス """
+
+    def __init__(self,freq):
+        self._clock = pygame.time.Clock()
+        self._freq = freq
+
+    def run(self):
+
+        pygame.init()
+        pygame.display.set_caption('ライントレース・シミュレーター')
+        screen = pygame.display.set_mode((800,600))
+        screen.fill(WHITE)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.update()
+            self._clock.tick(self._freq)
+
+def main():
+    mils = LFMils(freq = 10)
+    mils.run()
+
+if __name__ == '__main__':
+    main()
